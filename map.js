e73efbd77ba38427e9cd08becbb7ba65d1c1cc28
flother <flother@users.noreply.github.com>
2021-04-09 21:06:34 +0000

    Use Mapbox navigation guidance night map style

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = "pk.eyJ1IjoiZmxvdGhlciIsImEiOiJja25hcm85a3owcWlkMnBsYzJjd202cDE1In0.HuXg98Rjdh2KmSsReyqR8w";
 var map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/outdoors-v11",
+  style: "mapbox://styles/mapbox/navigation-guidance-night-v4",
   center: [-21.940928, 64.144789],
   zoom: 12,
 });
