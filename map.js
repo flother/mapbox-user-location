1426c8dacff9139a62dbafbd8a112730e9b1be37
flother <flother@users.noreply.github.com>
2021-04-09 20:39:09 +0000

    Initial commit

---
+++ map.js
@@ -0,0 +1,15 @@
+mapboxgl.accessToken = "pk.eyJ1IjoiZmxvdGhlciIsImEiOiJja25hcm85a3owcWlkMnBsYzJjd202cDE1In0.HuXg98Rjdh2KmSsReyqR8w";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/outdoors-v11",
+  center: [-21.940928, 64.144789],
+  zoom: 12,
+});
+map.addControl(
+  new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true,
+    },
+    trackUserLocation: true,
+  })
+);
