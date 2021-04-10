aa2d678d8091c7812c7fc9de654d97636bb3046f
flother <flother@users.noreply.github.com>
2021-04-10 17:01:56 +0000

    Use lighter version of navigation guidance style

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = "pk.eyJ1IjoiZmxvdGhlciIsImEiOiJja25hcm85a3owcWlkMnBsYzJjd202cDE1In0.HuXg98Rjdh2KmSsReyqR8w";
 var map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/navigation-guidance-night-v4",
+  style: "mapbox://styles/mapbox/navigation-guidance-day-v4",
   center: [-21.940928, 64.144789],
   zoom: 12,
 });
