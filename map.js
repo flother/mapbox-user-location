b5977c16cb78b73ca4dc339f746b61537445fc79
flother <flother@users.noreply.github.com>
2021-04-11 10:51:40 +0000

    Add comments to JavaScript code

--- map.js
+++ map.js
@@ -1,4 +1,17 @@
+// A simple test of the support for user-tracking in Mapbox GL JS v1.13.1. View
+// the demo at https://flother.github.io/mapbox-user-location/.
+//
+// https://docs.mapbox.com/mapbox-gl-js/api/
+// https://docs.mapbox.com/mapbox-gl-js/api/markers/#geolocatecontrol
+
+// This access token is limited to flother.github.io. Change it to use your own.
+//
+// https://account.mapbox.com/access-tokens/
 mapboxgl.accessToken = "pk.eyJ1IjoiZmxvdGhlciIsImEiOiJja25hcm85a3owcWlkMnBsYzJjd202cDE1In0.HuXg98Rjdh2KmSsReyqR8w";
+
+// Add a map to the web page. Its rendered in the <div> element with the id "map".
+//
+// https://docs.mapbox.com/mapbox-gl-js/api/map/
 var map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/navigation-guidance-day-v4",
@@ -6,6 +19,12 @@ var map = new mapboxgl.Map({
   zoom: 12,
 });
 
+// Geolocate the user and track their current location on the map using the
+// GeolocateControl. The map will pitch to 60° when geolocation is activated.
+//
+// https://docs.mapbox.com/mapbox-gl-js/example/locate-user/
+// https://docs.mapbox.com/mapbox-gl-js/api/markers/#geolocatecontrol
+// https://developer.mozilla.org/en-US/docs/Web/API/Geolocation
 let geolocate = new mapboxgl.GeolocateControl({
   positionOptions: {
     enableHighAccuracy: true,
@@ -23,6 +42,9 @@ geolocate.on("geolocate", (e) => {
   }
 });
 
+// Add a compass control to the map.
+//
+// https://docs.mapbox.com/mapbox-gl-js/api/markers/#navigationcontrol
 map.addControl(
   new mapboxgl.NavigationControl({
     showZoom: false,
@@ -30,4 +52,8 @@ map.addControl(
   }),
   "top-right"
 );
+// Add a scale bar to show the ratio of a distance on the map to the
+// corresponding distance on the ground.
+//
+// https://docs.mapbox.com/mapbox-gl-js/api/markers/#scalecontrol
 map.addControl(new mapboxgl.ScaleControl());
